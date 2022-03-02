cafcddff5ab6eda8f8c1d490e538e99a9d6c0af5
Leonardo Martins Schellenberg <leonardomartinsschellenberg@MacBook-Pro-de-MacOs.local>
2022-03-02 20:56:02 -0300

    Correção rotas

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import facebookImg from '../../assets/404.svg';
 import { Container } from './styles';
 import { FiChevronLeft } from 'react-icons/fi';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
