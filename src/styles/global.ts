8ec1355b448e2aa287d155d87b9ad30a9f94bc8b
Leonardo Martins Schellenberg <leonardomartinsschellenberg@MacBook-Pro-de-MacOs.local>
2022-03-07 14:52:42 -0300

    Prevent Zoom click input

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,9 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        -webkit-text-size-adjust: none;
+        text-size-adjust: none;
     }
 
     html{
